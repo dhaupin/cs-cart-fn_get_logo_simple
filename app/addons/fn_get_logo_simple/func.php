50264feb3efaefbe5335307bdd60f522630dad60
dhaupin <dhaupin@gmail.com>
2017-07-14 11:17:22 -0400

    Opt
    - ?s works just dandy in queries
    - check for object_type "logos"  in cscart_images_links table. Reason is that there can be multiple object_id's for different object types -- it is not unique column.

--- app/addons/fn_get_logo_simple/func.php
+++ app/addons/fn_get_logo_simple/func.php
@@ -10,11 +10,11 @@ function fn_get_logo_simple($company_id = 0, $type = 'theme') {
 	$theme = Settings::instance($company_id)->getValue('theme_name', '');
 	$layout_data = Layout::instance($company_id)->getDefault($theme);
 	
-	$logo_id = db_get_field("SELECT logo_id FROM ?:logos WHERE company_id = ?s AND type = ?s AND layout_id = ?i AND style_id = ?s", $company_id, $type, $layout_data['layout_id'], $layout_data['style_id']);
+	$logo_id = db_get_field("SELECT logo_id FROM ?:logos WHERE company_id = ?s AND type = ?s AND layout_id = ?s AND style_id = ?s", $company_id, $type, $layout_data['layout_id'], $layout_data['style_id']);
 	
-	$image_id = db_get_field("SELECT image_id FROM ?:images_links WHERE object_id = ?i", $logo_id);
+	$image_id = db_get_field("SELECT image_id FROM ?:images_links WHERE object_type = 'logos' AND object_id = ?s", $logo_id);
 	
 	$logo = fn_get_image($image_id, 'logos');
-
+	
 	return $logo;
 }
